24c0e0acd7081a316e42549327b5396ae9588c8d
vateseif <vateseif@gmail.com>
2023-06-13 02:37:54 +0200

    Add opendoor function

--- src/abstract_robot/gpt_robot.py
+++ src/abstract_robot/gpt_robot.py
@@ -24,16 +24,17 @@ SYSTEM_PROMPT_SIMPLE = """You are the cognitive center of a robot. This means, y
 
 The API at your disposal (the call function is in big letters):
  * EXPLORE()
- * PICKUP(objectname)
- * MOVETO(objectname)
- * PUTDOWN(objectname)
+ * PICKUP(keyname)
+ * MOVETO(keyname)
+ * PUTDOWN(keyname)
+ * OPENDOOR(doorname, keyname)
 
 call FINISHED as soon as you think you're done.
 
-Always make sure to explore everything. Because if you don't you might not be able to complete the task. Only call one API function at a time."""
+Always make sure to explore everything. Because if you don't you might not be able to complete the task. Only call one API function at a time and provide the argument correctly to within the function."""
 
 class GPTRobot():
-    def __init__(self, task_message, robot_explore, robot_pickup, robot_moveto, robot_putdown, finished):
+    def __init__(self, task_message, robot_explore, robot_pickup, robot_moveto, robot_putdown, robot_opendoor, finished):
         self.task_message = task_message
         self.messages = [
                     {"role": "system", "content": SYSTEM_PROMPT_SIMPLE},
@@ -43,6 +44,7 @@ class GPTRobot():
         self.robot_pickup = robot_pickup
         self.robot_moveto = robot_moveto
         self.robot_putdown = robot_putdown
+        self.robot_opendoor = robot_opendoor
         self.finished = finished
 
     def next_action(self, robot_answer=None):
@@ -69,7 +71,7 @@ class GPTRobot():
         return result
     
     def apply_message(self, message_string):
-        print("gpt message:" + message_string)
+        print('\033[91m' + message_string + '\033[0m')
         if "EXPLORE" in message_string:
             # EXPLORE(roomname), extract roomname
             # roomname = message_string.split("EXPLORE(")[1].split(")")[0]
@@ -86,6 +88,11 @@ class GPTRobot():
             # PUTDOWN(objectname), extract objectname
             objectname = message_string.split("PUTDOWN(")[1].split(")")[0]
             return self.robot_putdown(objectname)
+        if "OPENDOOR" in message_string:
+            # OPENDOOR(objectname), extract objectname
+            doorname = message_string.split("OPENDOOR(")[1].split(",")[0]
+            keyname = message_string.split(f"OPENDOOR({doorname}, ")[1].split(")")[0]
+            return self.robot_opendoor(doorname, keyname)
         if "FINISHED" in message_string:
             return self.finished()
         else:
